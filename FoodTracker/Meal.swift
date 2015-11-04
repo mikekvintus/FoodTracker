ba6859d678fd85a3e1f866585b7fd62c91cfbf5a
Mike Kvintus <mikekvintus@gmail.com>
2015-11-03 19:27:09 -0500

    Completed define you data model section

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,32 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Mike Kvintus on 11/1/15.
+//  Copyright © 2015 Mike Kvintus. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+
+}
